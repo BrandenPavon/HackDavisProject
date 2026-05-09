7088411cbf5b9c8fd71e864efb5d296d87e9b108
nholasek <nholasek@gmail.com>
2026-05-09 18:37:01 -0400

    Can confirm that MPU is being found via i2c

--- ArduinoCode/imu-Setup/imu-Setup.ino
+++ ArduinoCode/imu-Setup/imu-Setup.ino
@@ -4,10 +4,34 @@
 #define SCL_PIN 9
 
 void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
   Wire.begin(SDA_PIN, SCL_PIN);
+
+  Serial.println("I2C scanner starting...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  byte error, address;
+  int devicesFound = 0;
 
+  for (address = 1; address < 127; address++) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0) {
+      Serial.print("I2C device found at 0x");
+      if (address < 16) Serial.print("0");
+      Serial.println(address, HEX);
+      devicesFound++;
+    }
+  }
+
+  if (devicesFound == 0) {
+    Serial.println("No I2C devices found.");
+  }
+
+  Serial.println();
+  delay(2000);
 }
